4b4352b6ac5c3005a62b1dd12b0c1a4967e2fcf1
thomaspfresco <thomasfresco@live.com>
2023-09-11 16:28:28 +0100

    Fix Range

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let number = 0;
-let rangeNum = 9999;
+let rangeNum = 4000;
 let instant = 0;
 let instant2 = 0;
 let strAux;
@@ -44,6 +44,22 @@ function draw() {
 
 function genNum() {
     number = int(random(rangeNum));
+    while((number >= 131 && number <=200) 
+       || (number >= 2331 && number <=2350)
+       || (number >= 2361 && number <=2380)
+       || (number >= 2382 && number <=2390)
+       || (number >= 2399 && number <=2400)
+       || (number >= 2523 && number <=2750)
+       || (number >= 2752 && number <=2770)
+       || (number >= 2776 && number <=2810)
+       || (number >= 2841 && number <=2850)
+       || (number == 2821)
+       || (number >= 2828 && number <=2830)
+       || (number >= 2833 && number <=2840)
+       || (number >= 2901 && number <=3000)
+    ) {
+        number = int(random(rangeNum));
+    }
     strAux = number.toString();
     if (strAux.length == 1) str = "000" + strAux;
     else if (strAux.length == 2) str = "00" + strAux;
